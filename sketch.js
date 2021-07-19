5e32b3295be5483b5fe4a8ca811779cb9f7e3291
Abhinav7D-sketch <81948494+Abhinav7D-sketch@users.noreply.github.com>
2021-07-19 21:04:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var database;
+var player,form,game;
+var gameState=0;
+var playerCount;
+
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+}
+
+function draw(){
+  
+}
